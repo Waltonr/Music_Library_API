8871d5b51ea5d21418d1d6770d73c00769d86075
Waltonr <waltonr18126@gmail.com>
2022-05-06 12:45:01 -0400

    Registered Song Model with Admin Site + Completed POST and GET View Function for Song

--- song/views.py
+++ song/views.py
@@ -1,4 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
 
 
+@api_view(['GET', 'POST'])
+def song_list(request):
+    if request.method == 'GET':
+        song = Song.objects.all()
+        serializer = SongSerializer(song, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
