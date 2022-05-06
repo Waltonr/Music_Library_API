21c7bf457fe270307b925a27d9b48d5bc2181097
Waltonr <waltonr18126@gmail.com>
2022-05-06 11:19:12 -0400

    Modified Settings.py + Created Song App

--- music_library_project/settings.py
+++ music_library_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -44,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'music_library_project.urls'
@@ -112,7 +114,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 try:
-    from music_library_project import *
+    from music_library_project.local_settings import *
 except ImportError:
     pass
